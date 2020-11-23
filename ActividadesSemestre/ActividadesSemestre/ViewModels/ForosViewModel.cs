26e199a34669c847481f42162e6805e824b63cd7
Fabián Rojas <forf1997@hotmail.com>
2020-11-23 11:11:02 -0500

    Se recarga pantalla de foros al crear uno nuevo.

--- ActividadesSemestre/ActividadesSemestre/ViewModels/ForosViewModel.cs
+++ ActividadesSemestre/ActividadesSemestre/ViewModels/ForosViewModel.cs
@@ -10,33 +10,14 @@ using Xamarin.Forms;
 
 namespace ActividadesSemestre.ViewModels{
     public class ForosViewModel : INotifyPropertyChanged{
+
+        IMongoCollection<BsonDocument> forosCollection;
+
         public ForosViewModel(){
             Foros = new ObservableCollection<Foro>();
-            var forosCollection = App.ActividadesSemestreBD.GetCollection<BsonDocument>("Foros");
-            var mensajesCollection = App.ActividadesSemestreBD.GetCollection<BsonDocument>("MensajesForo");
+            CargarForos();
 
-            foreach (BsonDocument foro in forosCollection.Find(new BsonDocument()).ToList()){
-                int idForo = (int)foro["IdForo"];
-                var mensajes = mensajesCollection.Find(Builders<BsonDocument>.Filter.Eq("IdForo", idForo)).ToList();
-                var mensajesForo = new ObservableCollection<Mensaje>();
-                foreach (BsonDocument mensaje in mensajes){
-                    int idUsuario = (int)mensaje["IdUsuario"];
-                    mensajesForo.Add(new Mensaje{
-                        IdForo = idForo,
-                        IdUsuario = idUsuario,
-                        Fecha = (DateTime)mensaje["Fecha"],
-                        Texto = mensaje["Mensaje"].ToString(),
-                        NombreUsuario = App.ObtenerRemitente(idUsuario)
-                    });
-                }
-                Foros.Add(new Foro{
-                    IdForo = idForo,
-                    NombreForo = foro["NombreForo"].ToString(),
-                    Mensajes = mensajesForo
-                });
-            }
-
-            AbrirMensajesForo = new Command(async () =>{
+            AbrirMensajesForo = new Command(async () => {
                 if (ForoSeleccionado is null)
                     return;
 
@@ -62,6 +43,7 @@ namespace ActividadesSemestre.ViewModels{
                     };
                     forosCollection.InsertOne(document);
                     await Application.Current.MainPage.Navigation.PopModalAsync();
+                    CargarForos();
                 };
                 Button cancelarCreacion = new Button { Text = "Cancelar" };
                 cancelarCreacion.Clicked += async (object sender, EventArgs args) => {
@@ -73,10 +55,38 @@ namespace ActividadesSemestre.ViewModels{
                         nombreForo,
                         crearForo,
                         cancelarCreacion
-                    }
+                    },
+                    Padding = new Thickness{ Top = 10, Right = 30, Bottom = 10, Left = 30}
                 };
                 await Application.Current.MainPage.Navigation.PushModalAsync(new ContentPage { Content = CrearForoPage});
             });
+        }
+
+        private void CargarForos(){
+            Foros.Clear();
+            forosCollection = App.ActividadesSemestreBD.GetCollection<BsonDocument>("Foros");
+            var mensajesCollection = App.ActividadesSemestreBD.GetCollection<BsonDocument>("MensajesForo");
+            foreach (BsonDocument foro in forosCollection.Find(new BsonDocument()).ToList()){
+                int idForo = (int)foro["IdForo"];
+                var mensajes = mensajesCollection.Find(Builders<BsonDocument>.Filter.Eq("IdForo", idForo)).ToList();
+                var mensajesForo = new ObservableCollection<Mensaje>();
+                foreach (BsonDocument mensaje in mensajes){
+                    int idUsuario = (int)mensaje["IdUsuario"];
+                    mensajesForo.Add(new Mensaje
+                    {
+                        IdForo = idForo,
+                        IdUsuario = idUsuario,
+                        Fecha = (DateTime)mensaje["Fecha"],
+                        Texto = mensaje["Mensaje"].ToString(),
+                        NombreUsuario = App.ObtenerRemitente(idUsuario)
+                    });
+                }
+                Foros.Add(new Foro{
+                    IdForo = idForo,
+                    NombreForo = foro["NombreForo"].ToString(),
+                    Mensajes = mensajesForo
+                });
+            }
         }
 
         ObservableCollection<Foro> foros;
